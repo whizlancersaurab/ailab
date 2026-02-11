86df8fdf65febfda04a339437de3ca355922317e
saurabh <saurabh.agrahari@whizlancer.com>
2026-02-11 19:09:21 +0530

    resolve popup

--- frontend/src/handlePopUpmodal.ts
+++ frontend/src/handlePopUpmodal.ts
@@ -1,8 +1,10 @@
+import { Modal } from "bootstrap";
+
 export const handleModalPopUp = (elemId: string) => {
-  const modalElement = document.getElementById(elemId) as HTMLElement;
-  if (modalElement) {
-    // Use getOrCreateInstance instead of manually checking
-    const modalInstance = window.bootstrap.Modal.getOrCreateInstance(modalElement);
-    modalInstance.hide();
-  }
+  const modalElement = document.getElementById(elemId);
+  if (!modalElement) return;
+
+  const modalInstance = Modal.getOrCreateInstance(modalElement);
+  console.log(modalInstance)
+  modalInstance.hide();
 };
