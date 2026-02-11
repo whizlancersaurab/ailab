ee798ee4adcc03cad1b546517489afcc46be9ad7
saurabh <saurabh.agrahari@whizlancer.com>
2026-02-11 19:15:34 +0530

    fix iisue

--- frontend/src/handlePopUpmodal.ts
+++ frontend/src/handlePopUpmodal.ts
@@ -1,10 +1,8 @@
-import { Modal } from "bootstrap";
-
 export const handleModalPopUp = (elemId: string) => {
-  const modalElement = document.getElementById(elemId);
-  if (!modalElement) return;
-
-  const modalInstance = Modal.getOrCreateInstance(modalElement);
-  console.log(modalInstance)
-  modalInstance.hide();
+  const modalElement = document.getElementById(elemId) as HTMLElement;
+  if (modalElement) {
+    // Use getOrCreateInstance instead of manually checking
+    const modalInstance = window.bootstrap.Modal.getOrCreateInstance(modalElement);
+    modalInstance.hide();
+  }
 };
